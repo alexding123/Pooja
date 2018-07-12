9945e5230492703ce8582eea4cffd9576247dc96
Alexander Ding <alexding123@outlook.com>
2018-07-12 15:14:17 -0400

    Database half done

--- database.py
+++ database.py
@@ -1,3 +1,10 @@
+import pickle
+from pathlib import Path
+
+root = Path(".")
+database_path = root / "database.txt"
+print(database_path)
+
 def load_database(path):
     """ Loads a pickled dictionary that acts as the fingerprint databse
 
@@ -10,6 +17,13 @@ def load_database(path):
         -------
         dict((fn,fm,tm-tn)->list[(song_ID, tmatch)])
     """
+    if not path.exists():
+        err_msg = "The database path does not exist"
+        raise AssertionError(err_msg)
+    
+    with open(path, mode="rb") as f:
+        db = pickle.load(f)
+    return db
 
 def store_database(path, db):
     """ Stores a database (dictionary) as a pickled file
@@ -26,6 +40,12 @@ def store_database(path, db):
         -------
         dict((fn,fm,tm-tn)->list[(song_ID, tmatch)])
     """
+    if not path.exists():
+        err_msg = "The database path does not exist"
+        raise AssertionError(err_msg)
+    
+    with open(path, mode="wb") as f:
+        pickle.dump(db, f)
 
 def database():
     """ A fake constructor function that returns our database
@@ -36,4 +56,14 @@ def database():
     """
     return dict()
 
-def 
+def add_mp3(path, db):
+    """ Adds a song from a path into the database
+        
+        Parameters
+        ----------
+        path: pathlib.Path 
+            The path to the location where the song is
+        
+        db: dict
+            The database to be stored
+    """
