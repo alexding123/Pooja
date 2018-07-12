fb02b31d81eaa6bcac173d7b678f478a4cf682f7
denver-blake <denver.blake26@gmail.com>
2018-07-12 14:41:22 -0400

    wrote the peaks to fingerprint function

--- PeaksToFingerprints.py
+++ PeaksToFingerprints.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 
-
-
-def peaksToFingerprints(row,col):
+def peaks_to_fingerprints(row, col):
     """ Peaks to Fingerprints
-                Parameters
-                ----------
-                row: rows of the peaks - np.array
-                col: columns of the peaks - np.array
-                Returns
-                -------
-                fingerprints : list of ((fn, fn+i, tn+i - tn), tn) """
-
-
-    return fingerprint
+        Parameters
+        ----------
+        row: time bin of the peaks - np.array
+        col: frequency bin of the peaks - np.array
+        Returns
+        -------
+        fingerprints : list of ((fn, fn+i, tn+i - tn), tn) """
+    fanout = 10
+    fingerprints = []
+    for i, time in enumerate(row):
+        fingerprints.append(((col[i], col[i + n], row[i + n] - time), time) for n in np.arange(1, fanout))
+    return fingerprints
